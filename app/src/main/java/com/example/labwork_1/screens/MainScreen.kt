a1a80b6de1259bb2a30d6feac2b3f7e65372daae
Михеев Владислав <vlad.mixeev.1999@bk.ru>
2024-11-13 01:29:52 +0300

    Проект 1 - 3 видео

--- app/src/main/java/com/example/labwork_1/screens/MainScreen.kt
+++ app/src/main/java/com/example/labwork_1/screens/MainScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,7 +32,7 @@ import coil.compose.AsyncImage
 import com.example.labwork_1.R
 import com.example.labwork_1.ui.theme.BlueLight
 
-@Preview (showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun MainScreen() {
     androidx.compose.foundation.Image(
@@ -39,39 +41,101 @@ fun MainScreen() {
         modifier = Modifier
             .fillMaxSize()
             .alpha(0.7f),
-        contentScale = ContentScale.FillBounds)
+        contentScale = ContentScale.FillBounds
+    )
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(5.dp)
     ) {
-     Card(onClick = { /*TODO*/ },
-         modifier = Modifier.fillMaxWidth(),
-         colors = CardDefaults.cardColors(
-             contentColor = BlueLight
-         ),
+        Card(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                contentColor = BlueLight
+            ),
             elevation = CardDefaults.cardElevation(
-             defaultElevation = 0.dp),
-         shape = RoundedCornerShape(10.dp)
-     ) {
-        Column(modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+                defaultElevation = 0.dp
+            ),
+            shape = RoundedCornerShape(10.dp)
         ) {
-          Row(modifier =  Modifier.fillMaxWidth(),
-              horizontalArrangement = Arrangement.SpaceBetween
-          ) {
-              Text(text = "20 Jun 2024 13:00",
-                  modifier = Modifier.padding(top = 8.dp, start = 8.dp),
-                  style = TextStyle(fontSize = 15.sp),
-                  color = Color.White
-              )
-              AsyncImage(model = "https://cdn.weatherapi.com/weather/64x64/night/116.png",
-                  contentDescription =  "im2", modifier = Modifier
-                      .size(35.dp)
-                      .padding(top = 3.dp, end = 8.dp))
-          }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "20 Jun 2024 13:00",
+                            modifier = Modifier.padding(
+                                top = 8.dp,
+                                start = 8.dp
+                            ),
+                            style = TextStyle(fontSize = 15.sp),
+                            color = Color.White
+                        )
+                        AsyncImage(
+                            model = "https://cdn.weatherapi.com/weather/64x64/night/116.png",
+                            contentDescription = "im2", modifier = Modifier
+                                .size(35.dp)
+                                .padding(
+                                    top = 3.dp,
+                                    end = 8.dp
+                                )
+                        )
+                    }
+                }
+                Text(
+                    text = "Мadrid",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "23 C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Sunny",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(onClick = {
+
+                    }
+                    ) {
+                        Icon(painter = painterResource(id = R.drawable.ic_search) ,
+                            contentDescription = "im3",
+                            tint = Color.White
+                        )
+                    }
+                    Text(
+                        text = "23 C/12 C",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(onClick = {
+
+                    }
+                    ) {
+                        Icon(painter = painterResource(id = R.drawable.ic_cloud_sync) ,
+                            contentDescription = "im3",
+                            tint = Color.White
+                        )
+                    }
+                }
+            }
         }
-     }
     }
 }
