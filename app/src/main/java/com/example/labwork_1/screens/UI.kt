ea574bc22300acc19d98235f737d0768d39af75b
Михеев Владислав <vlad.mixeev.1999@bk.ru>
2024-11-13 02:18:19 +0300

    1 Проект - 5 видео

---
+++ app/src/main/java/com/example/labwork_1/screens/UI.kt
@@ -0,0 +1,69 @@
+package com.example.labwork_1.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.labwork_1.ui.theme.BlueLight
+
+@Preview
+@Composable
+fun ListItem() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 3.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = BlueLight
+            ),
+            elevation = CardDefaults.cardElevation(
+            defaultElevation = 0.dp
+            ),
+            shape = RoundedCornerShape(5.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(start = 8.dp,
+                        top = 5.dp,
+                        bottom = 5.dp)
+            ) {
+                Text(text = "12:00")
+                Text(text = "Sunny")
+            }
+            Text(text = "25 C",
+                color = Color.White,
+                style = TextStyle(fontSize = 25.sp)
+            )
+            AsyncImage(
+                model = "https://cdn.weatherapi.com/weather/64x64/night/116.png",
+                contentDescription = "im8", modifier = Modifier
+                    .size(35.dp)
+                    .padding(
+                        end = 8.dp
+                    )
+            )
+        }
+    }
+}
